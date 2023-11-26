ba05f50f458025767bbd8480df5a4699f9ee1b1a
danielbatres <danielbatresuser@gmail.com>
2023-11-26 15:29:17 -0600

    Imperative states with useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -6,7 +6,9 @@ function UseState({ name }) {
   const [state, setState] = useState({
     value: '',
     error: false,
-    loading: false
+    loading: false,
+    deleted: false,
+    confirmed: false,
   });
 
   useEffect(() => {
@@ -15,36 +17,71 @@ function UseState({ name }) {
         setState({
           ...state,
           error: state.value !== SECURITY_CODE, 
-          loading: false 
+          loading: false,
+          confirmed: state.value === SECURITY_CODE
         });
-      }, 2000);
+      }, 1500);
     }
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Delete {name}</h2>
-      <p>Please enter the security code</p>
-      {state.error && (
-        <p>Error: invalid code</p>
-      )}
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Delete {name}</h2>
+        <p>Please enter the security code</p>
+        {state.error && (
+          <p>Error: invalid code</p>
+        )}
 
-      {state.loading && (
-        <p>Loading...</p>
-      )}
+        {state.loading && (
+          <p>Loading...</p>
+        )}
 
-      <input 
-        placeholder="Security code" 
-        value={state.value}
-        onChange={event => setState({ ...state, value: event.target.value })}
-      />
-      <button
-        onClick={() => {
-          setState({ ...state, error: false, loading: true })
-        }}
-      >Verify</button>
-    </div>
-  );
+        <input 
+          placeholder="Security code" 
+          value={state.value}
+          onChange={event => setState({ ...state, value: event.target.value })}
+        />
+        <button
+          onClick={() => {
+            setState({ ...state, error: false, loading: true })
+          }}
+        >Verify</button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <p>Confirmation, are you sure?</p>
+        <button onClick={() => {
+          setState({ 
+            ...state, 
+            deleted: true 
+          });
+        }}>Yes, delete</button>
+        <button onClick={() => {
+          setState({ 
+            ...state, 
+            confirmed: false 
+          })
+        }}>No, I regretted it</button>
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <p>Deleted</p>
+        <button onClick={() => {
+          setState({ 
+            ...state, 
+            confirmed: false, 
+            deleted: false, 
+            value: '' 
+          });
+        }}>Reset, go back</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export { UseState }
