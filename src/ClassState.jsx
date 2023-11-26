cdc9ae95c4e03bd57ff6d6324c5432656f5fef24
danielbatres <danielbatresuser@gmail.com>
2023-11-26 02:30:28 -0600

    Life cycle methods in React.Component

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
   constructor(props) {
@@ -6,7 +7,24 @@ class ClassState extends React.Component {
     
     this.state = {
       error: true,
+      loading: false,
     };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("update");
+
+    if (!!this.state.loading) {
+      setTimeout(() => {
+        console.log("Validating");
+
+        this.setState({ loading: false });
+      }, 2000);
+    }
   }
 
   render() {
@@ -15,12 +33,14 @@ class ClassState extends React.Component {
         <h2>Delete {this.props.name}</h2>
         <p>Please enter the security code</p>
 
-        {this.state.error && (
-          <p>Error: invalid code</p>
-        )}
+        {this.state.error && <p>Error: invalid code</p>}
+
+        {this.state.loading && <Loading />}
 
         <input placeholder="Security code" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>Verify</button>
+        <button onClick={() => this.setState({ loading: true })}>
+          Verify
+        </button>
       </div>
     );
   }
