459b70b4ecf603a96b336d71a10d8de64013a745
danielbatres <danielbatresuser@gmail.com>
2023-11-26 13:34:49 -0600

    Composite states with React.Component

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      error: true,
+      value: '',
+      error: false,
       loading: false,
     };
   }
@@ -22,6 +25,8 @@ class ClassState extends React.Component {
       setTimeout(() => {
         console.log("Validating");
 
+        this.setState({ error: SECURITY_CODE !== this.state.value })
+
         this.setState({ loading: false });
       }, 2000);
     }
@@ -33,11 +38,17 @@ class ClassState extends React.Component {
         <h2>Delete {this.props.name}</h2>
         <p>Please enter the security code</p>
 
-        {this.state.error && <p>Error: invalid code</p>}
+        {(this.state.error && !this.state.loading) && <p>Error: invalid code</p>}
 
         {this.state.loading && <Loading />}
 
-        <input placeholder="Security code" />
+        <input 
+          placeholder="Security code" 
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+        />
         <button onClick={() => this.setState({ loading: true })}>
           Verify
         </button>
