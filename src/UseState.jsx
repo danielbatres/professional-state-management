9585ead5c761a69f4b100ce2bdc5392ee54a0b9c
danielbatres <danielbatresuser@gmail.com>
2023-11-26 02:17:18 -0600

    Effects with useEffect

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function UseState({ name }) {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   
+  useEffect(() => {
+    console.log("Starting effect")
+
+    if (!!loading) {
+      setTimeout(() => {
+        console.log("Validating");
+
+        setLoading(false);
+      }, 2000);
+    }
+
+    console.log("End effect");
+  }, [loading]);
+
   return (
     <div>
       <h2>Delete {name}</h2>
@@ -10,9 +25,14 @@ function UseState({ name }) {
       {error && (
         <p>Error: invalid code</p>
       )}
+
+      {loading && (
+        <p>Loading...</p>
+      )}
+
       <input placeholder="Security code" />
       <button
-        onClick={() => setError(!error)}
+        onClick={() => setLoading(true)}
       >Verify</button>
     </div>
   );
