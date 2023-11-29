d70a764568a72a59b55d69010592b13b6304f987
danielbatres <danielbatresuser@gmail.com>
2023-11-28 20:11:56 -0600

    Declarative states with useReducer

--- src/UseReducer.jsx
+++ src/UseReducer.jsx
@@ -1,3 +1,53 @@
+import React, { useEffect, useReducer } from "react";
+
+const SECURITY_CODE = "paradigma";
+
+function UseReducer({ name }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (!!state.loading) setTimeout(() => {
+      dispatch({ type: 'CONFIRM' })
+    }, 1500);
+  }, [state.loading]);
+
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Delete {name}</h2>
+        <p>Please enter the security code</p>
+        {state.error && <p>Error: invalid code</p>}
+
+        {state.loading && <p>Loading...</p>}
+
+        <input
+          placeholder="Security code"
+          value={state.value}
+          onChange={(event) => dispatch({ type: 'WRITE', payload: event.target.value })}
+        />
+        <button onClick={() => dispatch({ type: 'CHECK' })}>Verify</button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <p>Confirmation, are you sure?</p>
+        <button onClick={() => dispatch({ type: 'DELETE' })}>Yes, delete</button>
+        <button onClick={() => dispatch({ type: 'RESET' })}>No, I regretted it</button>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>Deleted</p>
+        <button onClick={() => dispatch({ type: "RESET" })}>
+          Reset, go back
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
 const initialState = {
   value: "",
   error: false,
@@ -22,7 +72,7 @@ const reducerSwitch = (state, action) => {
   return newState;
 };
 
-const reducerObject = (state) => ({
+const reducerObject = (state, payload) => ({
   'ERROR': {
     ...state,
     error: true,
@@ -30,11 +80,33 @@ const reducerObject = (state) => ({
   },
   'CHECK': {
     ...state,
+    error: false,
     loading: true,
+  },
+  'CONFIRM': {
+    ...state,
+    error: state.value !== SECURITY_CODE,
+    loading: false,
+    confirmed: state.value === SECURITY_CODE,
+  },
+  'WRITE': {
+    ...state,
+    value: payload
+  },
+  'DELETE': {
+    ...state,
+    deleted: true,
+  },
+  'RESET': {
+    ...state,
+    confirmed: false,
+    deleted: false,
+    value: "",
   },
 });
 
 const reducer = (state, action) => {
-  return reducerObject(state)[action.type] || state;
+  return reducerObject(state, action.payload)[action.type] || state;
 }
 
+export { UseReducer };
